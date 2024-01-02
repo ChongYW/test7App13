f0984dd314198b345c975644665ade5a44af7160
CYW <68589904+ChongYW@users.noreply.github.com>
2024-01-02 14:40:33 +0800

    v2

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel")
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 
+// Get all user.
 exports.getAllUsers = asyncHandler(async (_, res) => {
     const allUser = await User.find();
     if(allUser.length >= 1){
@@ -12,6 +14,7 @@ exports.getAllUsers = asyncHandler(async (_, res) => {
     }
 });
 
+// Create user.
 exports.createUser = asyncHandler(async (req, res) => {
     const {
         UserName,
@@ -22,14 +25,24 @@ exports.createUser = asyncHandler(async (req, res) => {
         Status
     } = req.body;
 
-    if (!UserName || !Password) {
+    if (!UserName || !Password || !Email) {
         res.status(400);
         throw new Error("Username and Password is required!");
     }
+    
+    const userAvailable = await User.findOne({Email});
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("Given email is already registered")
+    }
+
+    const hashedPassword = await bcrypt.hash(Password, 10);
+    console.log("Hashed password: ", hashedPassword);
 
     const saveUser = await User.create({
         UserName,
-        Password,
+        Password: hashedPassword,
         Phone,
         Email,
         Role,
@@ -39,6 +52,7 @@ exports.createUser = asyncHandler(async (req, res) => {
     res.status(201).json({message: `User create successfully!`});
 });
 
+// Get a user.
 exports.getUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -49,6 +63,7 @@ exports.getUser = asyncHandler(async (req, res) => {
     }
 })
 
+// Update a user.
 exports.updateUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -78,6 +93,7 @@ exports.updateUser = asyncHandler(async (req, res) => {
     }
 });
 
+// Deleted a user.
 exports.deleteUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -87,6 +103,30 @@ exports.deleteUser = asyncHandler(async (req, res) => {
 
     await User.deleteOne({_id: req.params.id});
     res.status(200).json({message: `${req.params.id} has been deletd!`});
+})
+
+// User login.
+exports.login = asyncHandler(async (req, res) => {
+    const {Email, Password} = req.body;
+
+    if (!Email || !Password) {
+        res.status(400);
+        throw new Error("Email or Password cant be empty");
+    }
+
+    const user = await User.findOne({Email});
+
+    if (!user) {
+        res.status(404);
+        throw new Error("Email not registered...");
+    }
+
+    if (await bcrypt.compare(Password, user.Password)) {
+        res.status(200).json({message: "Login success!"})
+    }else{
+        res.status(401);
+        throw new Error("Password incorrect!");
+    }
 })
 
 // module.exports = {getAllUser};
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -11,4 +11,6 @@ router.route("/").get(userController.getAllUsers).post(userController.createUser
 
 router.route("/:id").get(userController.getUser).put(userController.updateUser).delete(userController.deleteUser);
 
+router.route("/login").post(userController.login);
+
 module.exports = router;
